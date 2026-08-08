553009f9d0c8282e0fb1cd071f03e53551dad313
Andrei Kokorev <62149750+Dangerwind@users.noreply.github.com>
2026-08-08 08:50:32 +0000

    Добавить main

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(GeneratePassword(5))
-	fmt.Println(GeneratePassword(30))
+	// Проверка функции из условий задачи
+	fmt.Println(CheckPassword("abc"))         // Слабый пароль (оценка 1 из 5)
+	fmt.Println(CheckPassword("abcdefgh"))    // Слабый пароль (оценка 2 из 5)
+	fmt.Println(CheckPassword("abcdef1234"))  // Средний пароль (оценка 3 из 5)
+	fmt.Println(CheckPassword("Abcdef1234"))  // Надёжный пароль (оценка 4 из 5)
+	fmt.Println(CheckPassword("Abcdef123!")) 
 }
--- password_generator.go
+++ password_generator.go
@@ -1,17 +1,94 @@
 package main
+import (
+	"fmt"
+	"strings"
+)
 
 const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const digits    = "0123456789"
+const special   = "!@#$%^&*"
 
 
-func GeneratePassword(length int) string {
+func NextRandom(number int) int {
+	return (16807 * number) % 2147483647
+}
+
+
+func GeneratePassword(length, seed int, useUppercase, useDigits, useSpecial bool) string {
+	// Строчные буквы включены всегда
 	alphabet := lowercase
-	alphabetLen := len(alphabet) 
+
+	if useUppercase {
+		alphabet += uppercase
+	}
+
+	if useDigits {
+		alphabet += digits
+	}
+
+	if useSpecial {
+		alphabet += special
+	}
+
+	alphabetLen := len(alphabet)
 	result := ""
+	current := seed 
 
 	for i := 0; i < length; i++ {
-		index := i % alphabetLen 
-		result = result + string(alphabet[index]) 
+		current = NextRandom(current)
+		index := current % alphabetLen
+		result = result + string(alphabet[index])
 	}
 
 	return result
+}
+
+func CheckPassword(password string) string {
+	score := 0
+
+	if len(password) >= 8 {
+		score++
+	}
+
+	var hasLower, hasUpper, hasDigit, hasSpecial bool
+
+	for _, c := range password {
+		if c >= 'a' && c <= 'z' {
+			hasLower = true
+		} else if c >= 'A' && c <= 'Z' {
+			hasUpper = true
+		} else if c >= '0' && c <= '9' {
+			hasDigit = true
+		} else if strings.ContainsRune(special, c) {
+			hasSpecial = true
+		}
+	}
+
+	if hasLower {
+		score++
+	}
+	if hasUpper {
+		score++
+	}
+	if hasDigit {
+		score++
+	}
+	if hasSpecial {
+		score++
+	}
+
+	var verdict string
+	switch score {
+	case 0, 1, 2:
+		verdict = "Слабый"
+	case 3:
+		verdict = "Средний"
+	case 4:
+		verdict = "Надёжный"
+	case 5:
+		verdict = "Очень надёжный"
+	}
+
+	return fmt.Sprintf("%s пароль (оценка %d из 5)", verdict, score)
 }
